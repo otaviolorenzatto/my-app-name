7f857622f0360d94635e2a01b7a367dd20b0bf37
Otavio Augusto Lorenzatto <lorenzattoo@gmail.com>
2024-10-02 11:58:00 -0300

    Atualização do app

--- App.js
+++ App.js
@@ -1,73 +1,54 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-native';
+import { useState } from 'react';
+import { useFonts } from 'expo-font';
 import logo from './assets/logo.png';
 import fundomenu from './assets/fundomenu.png';
-import { ModalViagem } from './components/modal';
-import { useState } from 'react';
 import pessoa from './assets/pessoa.png';
 import casinha from './assets/casinha.png';
 
+import { ModalViagem } from './components/modal/pesquisaC.E.P';
 
 export default function App() {
-
-  const [mostraModal, setMostraModal] = useState(false)
+  const [mostraModal, setMostraModal] = useState(false);
 
   return (
     <View style={styles.container}>
-
       <View style={styles.header}>
-        <Image
-          source={logo}
-          style={styles.logo}
-        />
+        <Image source={logo} style={styles.logo} />
       </View>
 
       <Text style={styles.titulo}>traveler</Text>
       
-      <View >
-        <Image
-          source={fundomenu}
-          style={styles.imagemfundomenu}
-        />
+      <View>
+        <Image source={fundomenu} style={styles.imagemfundomenu} />
       </View>
 
       <Text style={styles.texto}>Qual seu próximo destino?</Text>
 
-      <TouchableOpacity style={styles.botao} onPress={ () => setMostraModal(true) }>
-      <Text style={styles.textobotao}>+ Adicionar nova viagem</Text>
+      <TouchableOpacity style={styles.botao} onPress={() => setMostraModal(true)}>
+        <Text style={styles.textobotao}>+ Adicionar nova viagem</Text>
       </TouchableOpacity>
       
       <View style={styles.linha}></View>
+      
       <View style={styles.rodapeContainer}>
+        <TouchableOpacity>
+          <View style={styles.casinhacontainer}>
+            <Image source={casinha} style={styles.casinha} />
+          </View>
+        </TouchableOpacity>
 
-      {/* Botão da casa */}
-      <TouchableOpacity>
-        <View style={styles.casinhacontainer}>
-          <Image
-            source={casinha} 
-            style={styles.casinha}
-          />
-        </View>
-      </TouchableOpacity>
-
-      {/* Botão da pessoa */}
-      <TouchableOpacity>
-        <View style={styles.pessoacontainer} >
-          <Image
-            source={pessoa} 
-            style={styles.pessoa}
-          />
-        </View>
-      </TouchableOpacity>
-        
-    </View>
+        <TouchableOpacity>
+          <View style={styles.pessoacontainer}>
+            <Image source={pessoa} style={styles.pessoa} />
+          </View>
+        </TouchableOpacity>
+      </View>
 
       <Modal visible={mostraModal} animationType='fade' transparent={true}> 
-        <ModalViagem fechar={() => setMostraModal(false)}/>
+        <ModalViagem fechar={() => setMostraModal(false)} />
       </Modal>
-
-      
-
-    </View> 
+    </View>
   );
 }
 
@@ -79,42 +60,35 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between', 
     paddingVertical: 20, 
   },
-
   header: {
     alignItems: 'center',
     marginTop: 50, 
   },
-
   logo: {
     width: 98,
     height: 83,
     marginBottom: 24,
   },
-
   titulo: {
     fontSize: 24,
     letterSpacing: 6.2,
-    fontFamily: 'Inter',
+    fontFamily: 'Inter',  // Aplicar a fonte Inter
     color: '#FFF',
   },
-
   imageContainer: {
     flex: 2, 
     justifyContent: 'center',
     alignItems: 'center',
   },
-
   imagemfundomenu: {
     width: 300,
     height: 300,
   },
-
   texto: {
     color: 'rgba(255,255,255,0.50)',
     fontSize: 22,
     marginBottom: 20,
   },
-
   botao: {
     backgroundColor: '#0E6EFF',
     width: 315,
@@ -123,26 +97,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
   textobotao: {
     color: '#FFF',
     fontSize: 18,
   },
-
   linha: {
     width: '90%',
     height: 1,
     backgroundColor: 'rgba(255,255,255,0.50)',
     marginTop: 130,
   },
-
   rodapeContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '80%', 
     paddingVertical: 10,
   },
-
   casinhacontainer: {
     backgroundColor: '#0A1A2F', 
     padding: 20,
@@ -150,26 +120,20 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     paddingHorizontal: 50,
   },
-
   pessoacontainer:{
     padding: 20,
     borderRadius: 10,
     marginBottom: 20,
     paddingHorizontal: 50,
   },
-
-  pessoa:{
+  pessoa: {
     width: 30,
     height: 30,
     tintColor: '#fff', 
   },
-
   casinha: {
     width: 30,
     height: 30,
     tintColor: '#0E6EFF', 
   },
-
-  
-  
 });
